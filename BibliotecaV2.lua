83318bc2e33f86e55b5ff349c55826cc36a574cd
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-05-23 18:57:26 -0300

    Update BibliotecaV2.lua

--- BibliotecaV2.lua
+++ BibliotecaV2.lua
@@ -10,7 +10,7 @@ local opciones = {
 
     {nombre = "AtravesarTodo V.1", codigo = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Nocolision.lua"))()'},
 
-    {nombre = "TPlayer", codigo = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/TpPlayers.lua "))()'},
+    {nombre = "TPlayer", codigo = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/TPplayers.lua"))()'},
 
     {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
 

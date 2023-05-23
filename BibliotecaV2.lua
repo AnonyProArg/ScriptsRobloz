5808923a8b71b07c15a6059c00f2e02e165953be
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-05-23 03:33:10 -0300

    Create BibliotecaV2.lua

---
+++ BibliotecaV2.lua
@@ -0,0 +1,475 @@
+local opciones = {
+
+    {nombre = "Opción 1", codigo = 'loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Dex-Explorer-v2-10490"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+{nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 2", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 3", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 4", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 5", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+    {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
+
+}
+
+-- Crear un objeto ScreenGui
+
+local gui = Instance.new("ScreenGui")
+
+gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+-- Crear un objeto Frame para el menú flotante
+
+local frame = Instance.new("Frame")
+
+frame.Size = UDim2.new(0, 500, 0, 300)
+
+frame.Position = UDim2.new(0.5, -250, 0.5, -150)
+
+frame.BackgroundColor3 = Color3.new(1, 1, 1)
+
+frame.BorderSizePixel = 2
+
+frame.Parent = gui
+
+-- Crear un objeto ScrollingFrame para contener los botones
+
+local scrollingFrame = Instance.new("ScrollingFrame")
+
+scrollingFrame.Size = UDim2.new(1, -10, 1, -10)
+
+scrollingFrame.Position = UDim2.new(0, 5, 0, 5)
+
+scrollingFrame.BackgroundTransparency = 1
+
+scrollingFrame.ScrollBarThickness = 6
+
+scrollingFrame.Parent = frame
+
+-- Crear un objeto GridLayout para organizar los botones en el ScrollingFrame
+
+local gridLayout = Instance.new("UIGridLayout")
+
+gridLayout.CellSize = UDim2.new(0, 120, 0, 30)
+
+gridLayout.CellPadding = UDim2.new(0, 10, 0, 10)
+
+gridLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+
+gridLayout.VerticalAlignment = Enum.VerticalAlignment.Top
+
+gridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+gridLayout.Parent = scrollingFrame
+
+-- Crear los botones en el ScrollingFrame
+
+for i, opcion in ipairs(opciones) do
+
+    local button = Instance.new("TextButton")
+
+    button.Size = UDim2.new(1, 0, 1, 0)
+
+    button.BackgroundColor3 = Color3.new(0, 0.7, 0)
+
+    button.TextColor3 = Color3.new(1, 1, 1)
+
+    button.Text = opcion.nombre
+
+    button.LayoutOrder = i
+
+    button.Parent = scrollingFrame
+
+    -- Función para ejecutar el código de la opción seleccionada y cerrar el menú
+
+    local function ejecutarCodigo()
+
+        loadstring(opcion.codigo)()
+
+        frame:Destroy()  -- Cerrar el menú flotante
+
+    end
+
+    -- Conectar la función al evento MouseButton1Click del botón
+
+    button.MouseButton1Click:Connect(ejecutarCodigo)
+
+end
+
+local isMobile = system.getInfo("platform") == "android" or system.getInfo("platform") == "ios"
+
+-- Función para simular eventos de puntero del mouse
+
+local function simulateMousePointer(event)
+
+    if isMobile then
+
+        -- Obtener las coordenadas táctiles
+
+        local touchX, touchY = event.x, event.y
+
+        
+
+        -- Simular eventos de puntero del mouse
+
+        if event.phase == "began" then
+
+            system.activate("mouse")
+
+            system.setMouseFocus(true)
+
+            system.moveMouse(touchX, touchY)
+
+            
+
+        elseif event.phase == "moved" then
+
+            system.moveMouse(touchX, touchY)
+
+            
+
+        elseif event.phase == "ended" then
+
+            system.activate("mouse")
+
+            system.setMouseFocus(false)
+
+        end
+
+    end
+
+    return true  -- Indica que el evento ha sido manejado correctamente
+
+end
+
+-- Manejo de eventos táctiles
+
+local function handleTouchEvents()
+
+    if isMobile then
+
+        Runtime:addEventListener("touch", simulateMousePointer)
+
+    end
+
+end
+
+-- Llamar a la función para manejar los eventos táctiles
+
+handleTouchEvents()
+
+gridLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(ajustarTamañoScrollingFrame)
+
+ajustarTamañoScrollingFrame()

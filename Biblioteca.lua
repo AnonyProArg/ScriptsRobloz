07daad117c58bf32af69b4bb411dbfc8b7abf1c7
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-05-28 03:13:35 -0300

    Create Biblioteca.lua

---
+++ Biblioteca.lua
@@ -0,0 +1,4 @@
+Comentar que script debería crear, especifica y comenta en el video :).. 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/BibliotecaV2.lua"))()
+

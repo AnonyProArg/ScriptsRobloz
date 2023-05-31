630ae3f9da2c33dc4d4a6ce0449b40177a811834
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-05-31 03:45:40 -0300

    Update BibliotecaV2.lua

--- BibliotecaV2.lua
+++ BibliotecaV2.lua
@@ -38,7 +38,7 @@ local opciones = {
 
     {nombre = "Tycoon-Milk", codigo = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Tycoon%20Milk.lua"))()'},
 
-    {nombre = "TP Botones T-Milk", codigo = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/TpTycoonMilk.lua"))()'},
+    {nombre = "TP Ubicaciones", codigo = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/TpTycoonMilk.lua"))()'},
 
     {nombre = "Opción 6", codigo = 'loadstring(game:HttpGet("###"))()'},
 

37b2c57b53919d8755986bb595b235b788a2fabf
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-05-31 03:44:44 -0300

    Update TpTycoonMilk.lua

--- TpTycoonMilk.lua
+++ TpTycoonMilk.lua
@@ -1,85 +1,187 @@
--- Variables globales
-local player = game.Players.LocalPlayer
-local savedPositions = {}
-local teleporting = false
+local locations = {}
 
--- Función para guardar la posición actual del jugador
-local function savePosition()
-    local currentPosition = player.Character.HumanoidRootPart.Position
-    table.insert(savedPositions, currentPosition)
-end
+local frame
 
--- Función para teletransportar al jugador a las ubicaciones guardadas
-local function teleportToSavedPositions()
-    if not teleporting then
-        teleporting = true
-        for i, position in ipairs(savedPositions) do
-            wait(2) -- Esperar 400 milisegundos entre cada teletransporte
-            player.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+local isMinimized = false
+
+local function CreateFloatingMenu()
+
+    frame = Instance.new("ScreenGui")
+
+    frame.Name = "FloatingMenu"
+
+    frame.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+    local frameBackground = Instance.new("Frame")
+
+    frameBackground.Size = UDim2.new(0, 200, 0, 250)
+
+    frameBackground.Position = UDim2.new(0.5, -100, 0.5, -125)
+
+    frameBackground.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+
+    frameBackground.BorderSizePixel = 0
+
+    frameBackground.Active = true
+
+    frameBackground.Draggable = true
+
+    frameBackground.Visible = true
+
+    frameBackground.Parent = frame
+
+    local title = Instance.new("TextLabel")
+
+    title.Size = UDim2.new(1, 0, 0, 30)
+
+    title.Text = "MyScript"
+
+    title.TextColor3 = Color3.fromRGB(255, 255, 255)
+
+    title.BackgroundTransparency = 1
+
+    title.Parent = frameBackground
+
+    local minimizeButton = Instance.new("TextButton")
+
+    minimizeButton.Size = UDim2.new(0, 30, 0, 30)
+
+    minimizeButton.Position = UDim2.new(1, -60, 0, 0)
+
+    minimizeButton.Text = "-"
+
+    minimizeButton.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+
+    minimizeButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+
+    minimizeButton.Parent = frameBackground
+
+    minimizeButton.MouseButton1Click:Connect(function()
+
+        isMinimized = not isMinimized
+
+        frameBackground.Visible = not isMinimized
+
+    end)
+
+    local maximizeButton = minimizeButton:Clone()
+
+    maximizeButton.Position = UDim2.new(1, -30, 0, 0)
+
+    maximizeButton.Text = "+"
+
+    maximizeButton.Parent = frameBackground
+
+    maximizeButton.Visible = false
+
+    maximizeButton.MouseButton1Click:Connect(function()
+
+        isMinimized = false
+
+        frameBackground.Visible = true
+
+        maximizeButton.Visible = false
+
+    end)
+
+    local closeButton = Instance.new("TextButton")
+
+    closeButton.Size = UDim2.new(0, 150, 0, 30)
+
+    closeButton.Position = UDim2.new(0.5, -75, 0.2, 0)
+
+    closeButton.Text = "Cerrar Menú"
+
+    closeButton.BackgroundColor3 = Color3.fromRGB(128, 128, 128)
+
+    closeButton.Parent = frameBackground
+
+    closeButton.MouseButton1Click:Connect(function()
+
+        frame:Destroy()
+
+    end)
+
+    local saveButton = closeButton:Clone()
+
+    saveButton.Position = UDim2.new(0.5, -75, 0.4, 0)
+
+    saveButton.Text = "Guardar Ubicación"
+
+    saveButton.BackgroundColor3 = Color3.fromRGB(70, 130, 180)
+
+    saveButton.Parent = frameBackground
+
+    saveButton.MouseButton1Click:Connect(function()
+
+        local player = game.Players.LocalPlayer
+
+        local character = player.Character
+
+        local humanoidRootPart = character and character:FindFirstChild("HumanoidRootPart")
+
+        if humanoidRootPart then
+
+            local position = humanoidRootPart.Position
+
+            table.insert(locations, position)
+
         end
-        teleporting = false
-    end
+
+    end)
+
+    local resetButton = saveButton:Clone()
+
+    resetButton.Position = UDim2.new(0.5, -75, 0.6, 0)
+
+    resetButton.Text = "Reset Ubicaciones"
+
+    resetButton.BackgroundColor3 = Color3.fromRGB(220, 20, 60)
+
+    resetButton.Parent = frameBackground
+
+    resetButton.MouseButton1Click:Connect(function()
+
+        locations = {}
+
+    end)
+
+    local tpButton = saveButton:Clone()
+
+    tpButton.Position = UDim2.new(0.5, -75, 0.8, 0)
+
+    tpButton.Text = "TP Ubicaciones"
+
+    tpButton.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+
+    tpButton.Parent = frameBackground
+
+    tpButton.MouseButton1Click:Connect(function()
+
+        local player = game.Players.LocalPlayer
+
+        local character = player.Character
+
+        local humanoidRootPart = character and character:FindFirstChild("HumanoidRootPart")
+
+        if humanoidRootPart then
+
+            for i, position in ipairs(locations) do
+
+                humanoidRootPart.Position = position
+
+                wait(1) -- Espera 1 segundo antes de teleportarse a la siguiente ubicación
+
+            end
+
+        end
+
+    end)
+
+    frameBackground.Visible = not isMinimized
+
+    maximizeButton.Visible = isMinimized
+
 end
 
--- Función principal
-local function main()
-    -- Crear la GUI si no existe
-    local gui = player.PlayerGui:FindFirstChild("TeleportGui")
-    if not gui then
-        gui = Instance.new("ScreenGui")
-        gui.Name = "TeleportGui"
-        gui.Parent = player.PlayerGui
-
-        local button1 = Instance.new("TextButton")
-        button1.Size = UDim2.new(0, 100, 0, 50)
-        button1.Position = UDim2.new(0, 10, 0, 10)
-        button1.Text = "Guardar posición 1"
-        button1.Parent = gui
-
-        local button2 = Instance.new("TextButton")
-        button2.Size = UDim2.new(0, 100, 0, 50)
-        button2.Position = UDim2.new(0, 10, 0, 70)
-        button2.Text = "Guardar posición 2"
-        button2.Parent = gui
-
-        local button3 = Instance.new("TextButton")
-        button3.Size = UDim2.new(0, 100, 0, 50)
-        button3.Position = UDim2.new(0, 10, 0, 130)
-        button3.Text = "Guardar posición 3"
-        button3.Parent = gui
-
-        local teleportButton = Instance.new("TextButton")
-        teleportButton.Size = UDim2.new(0, 100, 0, 50)
-        teleportButton.Position = UDim2.new(0, 10, 0, 190)
-        teleportButton.Text = "Teletransportar"
-        teleportButton.Parent = gui
-
-        -- Eventos de los botones
-        button1.MouseButton1Click:Connect(function()
-            savePosition()
-        end)
-
-        button2.MouseButton1Click:Connect(function()
-            savePosition()
-        end)
-
-        button3.MouseButton1Click:Connect(function()
-            savePosition()
-        end)
-
-        teleportButton.MouseButton1Click:Connect(function()
-            teleportToSavedPositions()
-        end)
-    end
-end
-
--- Ejecutar la función principal
-main()
-
--- Crear bucle para el botón "Teletransportar"
-local teleportButton = player.PlayerGui.TeleportGui:WaitForChild("TeleportButton")
-while true do
-    teleportButton.MouseButton1Click:Wait()
-    teleportToSavedPositions()
-    wait(10)
-end
+CreateFloatingMenu()

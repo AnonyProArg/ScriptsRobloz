5b2b8bb5364939f706792a5736fc842aebd8e213
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-01 22:18:27 -0300

    Actualizar Passes.lua

--- Passes.lua
+++ Passes.lua
@@ -1,1 +1,19 @@
-local gamePassesPath = game.Players.LocalPlayer.GamePasses -- Ruta de la ubicación de los archivos local function changeValueToTrue(object) if object:IsA("ValueBase") then object.Value = true end end local function iterateAndChangeValue(folder) for _, child in ipairs(folder:GetChildren()) do changeValueToTrue(child) if child:IsA("Folder") then iterateAndChangeValue(child) end end end -- Cambiar los valores de "value" a "true" en todos los archivos iterateAndChangeValue(gamePassesPath)
+local gamePassesPath = game.Players.LocalPlayer.GamePasses -- Ruta de la ubicación de los archivos
+
+local function changeValueToTrue(object)
+    if object:IsA("ValueBase") then
+        object.Value = true
+    end
+end
+
+local function iterateAndChangeValue(folder)
+    for _, child in ipairs(folder:GetChildren()) do
+        changeValueToTrue(child)
+        if child:IsA("Folder") then
+            iterateAndChangeValue(child)
+        end
+    end
+end
+
+-- Cambiar los valores de "value" a "true" en todos los archivos
+iterateAndChangeValue(gamePassesPath)

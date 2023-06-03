0de75f89068eb765375a5eb1c0c6ad8952681fe8
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-03 20:48:57 -0300

    Actualizar color o die map1.lua

--- color o die map1.lua
+++ color o die map1.lua
@@ -1,1 +1,11 @@
+local directorio = game.Workspace.GameplayParts 
+local nombresObjetosEliminar = {"Color_Blue", "Color_Green","Color_Pink","Color_Purple","Color_Orange","Color_Teal","Color_Yellow"} 
 
+local objetosAEliminar = directorio:GetDescendants()
+for _, objeto in ipairs(objetosAEliminar) do
+    for _, nombreObjetoEliminar in ipairs(nombresObjetosEliminar) do
+        if objeto.Name == nombreObjetoEliminar then
+            objeto:Destroy()
+        end
+    end
+end

1871b01a647844b318f6cd90e177cc2d19185244
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-20 21:24:04 -0300

    Actualizar Exe5.lua

--- Pastelería/Exe5.lua
+++ Pastelería/Exe5.lua
@@ -1,5 +1,8 @@
+while wait(0.1) do
+   workspace.Plots.Plot5.Shelf.Info:FireServer()
+end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/Posici%C3%B3n5.lua"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/Spam5.lua"))()
 
 

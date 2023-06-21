7ec0a4a80b2207b2dfe3dd9aa561e82313f69551
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-21 08:36:07 -0300

    Actualizar Laundry4.lua

--- Pastelería/Laundry4.lua
+++ Pastelería/Laundry4.lua
@@ -1,15 +1,11 @@
 
+
 local washingMachines = workspace.Plots.Plot4.WashingMachines:GetChildren()
 
-local startWashingScript = [[
-    while wait() do
-        game:GetService("ReplicatedStorage").Events.LoadWashingMachine:FireServer(workspace.Plots.Plot4.WashingMachines["%s"])
-    end
-]]
-
 local function startWashingMachine(washingMachine)
-    local script = string.format(startWashingScript, washingMachine.Name)
-    loadstring(script)()
+    while wait() do
+        game:GetService("ReplicatedStorage").Events.LoadWashingMachine:FireServer(washingMachine)
+    end
 end
 
 for i, washingMachine in ipairs(washingMachines) do
@@ -22,17 +18,8 @@ local function updateClothingList()
     return workspace.Debris.Clothing:GetChildren()
 end
 
-local invokeClothingScript = [[
-    local args = {
-        [1] = workspace.Debris.Clothing:FindFirstChild("%s")
-    }
-    
-    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("GrabClothing"):FireServer(unpack(args))
-]]
-
 local function invokeClothing(clothingName)
-    local script = string.format(invokeClothingScript, clothingName)
-    loadstring(script)()
+    game:GetService("ReplicatedStorage").Events.GrabClothing:FireServer(workspace.Debris.Clothing:FindFirstChild(clothingName))
 end
 
 local function invokeClothingProcess()
@@ -41,7 +28,7 @@ local function invokeClothingProcess()
     
     while true do
         for i = 1, #clothingList, 20 do
-            local endIndex = math.min(i + 4, #clothingList)
+            local endIndex = math.min(i + 8, #clothingList)
             
             for j = i, endIndex do
                 invokeClothing(clothingList[j].Name)
@@ -61,29 +48,48 @@ end
 
 spawn(invokeClothingProcess)
 
-local stopWashingScript = [[
-    local args = {
-        [1] = workspace.Plots.Plot4.WashingMachines:FindFirstChild("%s")
-    }
-    
-    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("UnloadWashingMachine"):FireServer(unpack(args))
-]]
+local function stopWashingMachine(washingMachine)
+    while true do
+        game:GetService("ReplicatedStorage").Events.UnloadWashingMachine:FireServer(washingMachine)
+        wait()
+    end
+end
 
 for i, washingMachine in ipairs(washingMachines) do
     spawn(function()
-        while true do
-            local script = string.format(stopWashingScript, washingMachine.Name)
-            loadstring(script)()
-            wait()
-        end
+        stopWashingMachine(washingMachine)
     end)
 end
 
 spawn(function()
     while true do
         for i = 1, 5 do
-            game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("DropClothesInChute"):FireServer()
+            game:GetService("ReplicatedStorage").Events.DropClothesInChute:FireServer()
         end
         wait(1)
     end
 end)
+
+local walkPositions = {
+    Vector3.new(-131.265, 5.57671, -43.5038,
+    Vector3.new(-106.621, 5.57671, -11.1147))
+}
+
+local function walkToPosition(position)
+    local humanoid = game.Players.LocalPlayer.Character.Humanoid
+    humanoid:MoveTo(position)
+    humanoid.MoveToFinished:Wait()
+    wait(1)
+end
+
+while true do
+    -- Ida
+    for _, position in ipairs(walkPositions) do
+        walkToPosition(position)
+    end
+    
+    -- Vuelta
+    for i = #walkPositions, 1, -1 do
+        walkToPosition(walkPositions[i])
+    end
+end

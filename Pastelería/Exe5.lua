4775f264f1bc41609247d1b09ae2f1a90a712105
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-20 21:42:36 -0300

    Actualizar Exe5.lua

--- Pastelería/Exe5.lua
+++ Pastelería/Exe5.lua
@@ -1,5 +1,15 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/Posici%C3%B3n5.lua"))()
+local spamThread = spawn(function()
+   while wait(0.1) do
+      workspace.Plots.Plot5.Shelf.Info:FireServer()
+   end
+end)
 
-while wait(0.1) do
-   workspace.Plots.Plot5.Shelf.Info:FireServer()
+local success, script = pcall(function()
+   return game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/Posici%C3%B3n5.lua")
+end)
+
+if success then
+   loadstring(script)()
+else
+   print("Error al obtener el script desde la URL")
 end

b95ef5cdb96eb35432d75e7326854a1fd1b4b5e0
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-21 21:14:28 -0300

    Actualizar Nocolision.lua

--- Nocolision.lua
+++ Nocolision.lua
@@ -1,7 +1,19 @@
 local player = game.Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 
+local function enableAutoJump()
+    local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+    if humanoid then
+        humanoid.AutoJumpEnabled = true
+    end
+end
+
 local function disableCollisionExceptFloor()
+    local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+    if humanoid then
+        humanoid.AutoJumpEnabled = false
+    end
+
     local parts = player.Character:GetDescendants()
     for _, part in ipairs(parts) do
         if part:IsA("BasePart") and part ~= workspace.Terrain then
@@ -11,6 +23,10 @@ local function disableCollisionExceptFloor()
 end
 
 local isCollisionDisabled = false
+
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = playerGui
+
 local disableCollisionButton = Instance.new("TextButton")
 disableCollisionButton.Name = "DisableCollisionButton"
 disableCollisionButton.Text = "Quitar Colisión"
@@ -18,7 +34,16 @@ disableCollisionButton.Size = UDim2.new(0, 180, 0, 50)
 disableCollisionButton.Position = UDim2.new(0.5, -90, 0, 20)
 disableCollisionButton.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
 disableCollisionButton.TextColor3 = Color3.new(1, 1, 1)
-disableCollisionButton.Parent = playerGui
+disableCollisionButton.Parent = screenGui
+
+local closeButton = Instance.new("TextButton")
+closeButton.Name = "CloseButton"
+closeButton.Text = "Cerrar Menú"
+closeButton.Size = UDim2.new(0, 180, 0, 50)
+closeButton.Position = UDim2.new(0.5, -90, 0, 80)
+closeButton.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
+closeButton.TextColor3 = Color3.new(1, 1, 1)
+closeButton.Parent = screenGui
 
 disableCollisionButton.Activated:Connect(function()
     if not isCollisionDisabled then
@@ -28,22 +53,16 @@ disableCollisionButton.Activated:Connect(function()
             disableCollisionExceptFloor()
             wait(0.001)
         end
+        enableAutoJump()  -- Activar salto automático al salir del menú
+        player.Character.Humanoid.Jump = true  -- Permitir saltar una vez al presionar el botón de salto
     else
         isCollisionDisabled = false
         disableCollisionButton.Text = "Quitar Colisión"
     end
 end)
 
-local closeButton = Instance.new("TextButton")
-closeButton.Name = "CloseButton"
-closeButton.Text = "Cerrar Menú"
-closeButton.Size = UDim2.new(0, 180, 0, 50)
-closeButton.Position = UDim2.new(0.5, -90, 0, 80)
-closeButton.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
-closeButton.TextColor3 = Color3.new(1, 1, 1)
-closeButton.Parent = playerGui
-
 closeButton.Activated:Connect(function()
-    disableCollisionButton:Destroy()
-    closeButton:Destroy()
+    screenGui:Destroy()
+    enableAutoJump()  -- Activar salto automático al cerrar el menú
+    player.Character.Humanoid.Jump = true  -- Permitir saltar una vez al presionar el botón de salto
 end)

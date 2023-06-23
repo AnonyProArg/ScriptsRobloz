a325c6b366b9a4676f472de4e6337c569c7e42ad
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-23 20:16:29 -0300

    Actualizar Cafe1.lua

--- Pastelería/Cafe1.lua
+++ Pastelería/Cafe1.lua
@@ -1,1 +1,9 @@
+local spamThread = spawn(function()
+   while wait(0.1) do
+      workspace.Plots.Plot1.Shelf.Info:FireServer()
+   end
+end)
 
+local success, script = pcall(function()
+   return game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/ExeC1.lua")
+end)

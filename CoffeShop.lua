a4337f597af01f9fc5894f054dc8d3f0e918daef
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-23 22:08:56 -0300

    Actualizar CoffeShop.lua

--- CoffeShop.lua
+++ CoffeShop.lua
@@ -1,69 +1,46 @@
+-- Crear el ScreenGUI
 local gui = Instance.new("ScreenGui")
+gui.Name = "CoffeeShopGUI"
 gui.Parent = game.Players.LocalPlayer.PlayerGui
-workspace.CurrentCamera.CameraType = Enum.CameraType.Fixed
 
--- Add button 1
-local Button1 = Instance.new("TextButton")
-Button1.Parent = gui
-Button1.Position = UDim2.new(0, 40, 0, 10) -- Ajusta la posición del botón
-Button1.Size = UDim2.new(0, 100, 0, 30) -- Ajusta el tamaño del botón
-Button1.Text = "P1"
-Button1.Activated:Connect(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe1.lua"))()
-    destroyUI()
-end)
+-- Función para destruir el GUI
+local function destroyUI()
+    gui:Destroy()
+end
 
--- Add button 2
-local Button2 = Instance.new("TextButton")
-Button2.Parent = gui
-Button2.Position = UDim2.new(0, 40, 0, 50) -- Ajusta la posición del botón
-Button2.Size = UDim2.new(0, 100, 0, 30) -- Ajusta el tamaño del botón
-Button2.Text = "P2"
-Button2.Activated:Connect(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe2.lua"))()
-    destroyUI()
-end)
+-- Función para cargar y ejecutar los scripts
+local function loadScript(url)
+    local success, script = pcall(function()
+        return loadstring(game:HttpGet(url))
+    end)
+    
+    if success then
+        script()
+        destroyUI()
+    else
+        warn("Error al cargar el script:", script)
+    end
+end
 
--- Add button 3
-local Button3 = Instance.new("TextButton")
-Button3.Parent = gui
-Button3.Position = UDim2.new(0, 40, 0, 90) -- Ajusta la posición del botón
-Button3.Size = UDim2.new(0, 100, 0, 30) -- Ajusta el tamaño del botón
-Button3.Text = "P3"
-Button3.Activated:Connect(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe3.lua"))()
-    destroyUI()
-end)
+-- Crear los botones
 
--- Add button 4
-local Button4 = Instance.new("TextButton")
-Button4.Parent = gui
-Button4.Position = UDim2.new(0, 40, 0, 130) -- Ajusta la posición del botón
-Button4.Size = UDim2.new(0, 100, 0, 30) -- Ajusta el tamaño del botón
-Button4.Text = "P4"
-Button4.Activated:Connect(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe4.lua"))()
-    destroyUI()
-end)
+local buttonsData = {
+    {Text = "P1", ScriptURL = "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe1.lua"},
+    {Text = "P2", ScriptURL = "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe2.lua"},
+    {Text = "P3", ScriptURL = "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe3.lua"},
+    {Text = "P4", ScriptURL = "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe4.lua"},
+    {Text = "P5", ScriptURL = "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe5.lua"},
+    {Text = "P6", ScriptURL = "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe6.lua"}
+}
 
--- Add button 5
-local Button5 = Instance.new("TextButton")
-Button5.Parent = gui
-Button5.Position = UDim2.new(0, 40, 0, 170) -- Ajusta la posición del botón
-Button5.Size = UDim2.new(0, 100, 0, 30) -- Ajusta el tamaño del botón
-Button5.Text = "P5"
-Button5.Activated:Connect(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe5.lua"))()
-    destroyUI()
-end)
-
--- Add button 6
-local Button6 = Instance.new("TextButton")
-Button6.Parent = gui
-Button6.Position = UDim2.new(0, 40, 0, 210) -- Ajusta la posición del botón
-Button6.Size = UDim2.new(0, 100, 0, 30) -- Ajusta el tamaño del botón
-Button6.Text = "P6"
-Button6.Activated:Connect(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pastelería/Cafe6.lua"))()
-    destroyUI()
-end)
+for i, buttonData in ipairs(buttonsData) do
+    local button = Instance.new("TextButton")
+    button.Name = "Button"..i
+    button.Parent = gui
+    button.Position = UDim2.new(0, 10, 0, (i - 1) * 40 + 10)
+    button.Size = UDim2.new(0, 100, 0, 30)
+    button.Text = buttonData.Text
+    button.Activated:Connect(function()
+        loadScript(buttonData.ScriptURL)
+    end)
+end

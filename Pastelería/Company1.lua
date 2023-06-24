5a072828f1cb55b2af46f544a237f4b79239a71a
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-24 16:15:05 -0300

    Actualizar Company1.lua

--- Pastelería/Company1.lua
+++ Pastelería/Company1.lua
@@ -18,8 +18,33 @@ local function collectMoneyLoop()
     end
 end
 
+local function clickMoneyLoop(amount, buttonType, instanceType)
+    while true do
+        local args = {
+            [1] = amount,
+            [2] = buttonType,
+            [3] = instanceType
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("ClickMoney"):InvokeServer(unpack(args))
+        wait(0.15)
+    end
+end
+
+local function collectMoneyLoop()
+    while true do
+        game:GetService("ReplicatedStorage").CollectMoney:InvokeServer()
+        wait(2)
+    end
+end
+
 spawn(function()
-    clickMoneyLoop(400, "TextButton", "Instance")
+    clickMoneyLoop(74, "TextButton", "Instance")
 end)
+
+spawn(function()
+    clickMoneyLoop(74, "TextButton", "Instance")
+end)
+
 
 spawn(collectMoneyLoop)

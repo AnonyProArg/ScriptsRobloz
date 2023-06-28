cd03576149af1e943d4ef5c93dfded246b2805e5
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-28 07:22:49 -0300

    Actualizar CéspedCaminar.lua

--- CéspedCaminar.lua
+++ CéspedCaminar.lua
@@ -1,4 +1,14 @@
+-- Obtener el personaje y su Humanoid
+local character = game:GetService("Players").LocalPlayer.Character
+local humanoid = character:WaitForChild("Humanoid")
+
+-- Función para mover al personaje a una posición
+local function walkToPosition(position)
+    humanoid:MoveTo(position)
+end
+
+-- Bucle infinito para mover al personaje
 while true do
-walkToPosition(Vector3.new(806.736, 10.1994, -318.83))
-wait(1)
+    walkToPosition(Vector3.new(806.736, 10.1994, -318.83))
+    wait(1)
 end

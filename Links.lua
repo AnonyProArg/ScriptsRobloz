63b82812e62608ec500bfe0227204dabadb3389a
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-29 18:27:01 -0300

    Update Links.lua

--- Links.lua
+++ Links.lua
@@ -1,4 +1,4 @@
 
-https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua
-https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/%2B10DeAltura.lua
-https://github.com/AnonyProArg/ScriptsRobloz/blob/main/Generaled/CaminarPlataforma.lua
+ESP Players:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua
+Plataforma+10:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/%2B10DeAltura.lua
+Caminar sobre plataformas:https://github.com/AnonyProArg/ScriptsRobloz/blob/main/Generaled/CaminarPlataforma.lua

d5c06696aa49c220e12c15e62c0264959a4841a5
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-29 18:34:43 -0300

    Actualizar Links.lua

--- Links.lua
+++ Links.lua
@@ -1,4 +1,3 @@
-
-ESP Players:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua
-Plataforma+10:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/%2B10DeAltura.lua
-Caminar sobre plataformas:https://github.com/AnonyProArg/ScriptsRobloz/blob/main/Generaled/CaminarPlataforma.lua
+ESS Players:loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua"))()
+Plataforma+10:loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/%2B10DeAltura.lua))()
+Caminar sobre plataformas:loadstring(game:HttpGet("https://github.com/AnonyProArg/ScriptsRobloz/blob/main/Generaled/CaminarPlataforma.lua"))()

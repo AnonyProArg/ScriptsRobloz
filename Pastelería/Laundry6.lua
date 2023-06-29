5fb3f979b18c9c2fe5fda4fcd6397366a5849f06
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-28 21:21:36 -0300

    Actualizar Laundry6.lua

--- Pastelería/Laundry6.lua
+++ Pastelería/Laundry6.lua
@@ -1,4 +1,4 @@
-local washingMachines = workspace.Plots.Plot4.WashingMachines:GetChildren()
+local washingMachines = workspace.Plots.Plot6.WashingMachines:GetChildren()
 
 local function startWashingMachine(washingMachine)
     while wait() do

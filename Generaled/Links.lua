bb94f78f561872a5ab77e6c5e9e5d2a65af420b8
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-30 09:26:37 -0300

    Actualizar Links.lua

--- Generaled/Links.lua
+++ Generaled/Links.lua
@@ -1,7 +1,8 @@
 
   local Links = {
         "true:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/ModoEspectador.lua",
-        "true:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caminar%20en%20Plataforma.lua",
+
+"true:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caminar%20en%20Plataforma.lua",
 
 "true:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua",
   

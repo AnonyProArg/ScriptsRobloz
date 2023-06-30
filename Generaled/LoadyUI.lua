1d862033b700a37a28bf9a79f0d7bc27c3d1e3bb
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-30 06:38:51 -0300

    Actualizar LoadyUI.lua

--- Generaled/LoadyUI.lua
+++ Generaled/LoadyUI.lua
@@ -1,12 +1,14 @@
-local Library = loadstring(game:HttpGet("https://bitbucket.org/cat__/uis/raw/bb4228f2f970aad3c4a901161da5e678fb8e14ef/Module"))() 
+local Library = loadstring(game:HttpGet("https://bitbucket.org/cat__/uis/raw/Loady/Module"))() 
 local Window = Library:Window("Loady", function(Script)
-print('Loaded', Script)
+    loadstring(game:HttpGet(Script))()
 end)
 Window:Game({
     Status = true,
-    Script = "Hi",
-    Place = 3101667897
+    Script = "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/OneCreatorX%203.lua",
+    UpdateDate = Window:GitDate("AnonyProArg","ScriptsRobloz","OneCreatorX%203.lua"),
+    Custom = {
+        Name = "Hola" 
+    } 
 })
 
 Window:Initiate()
-

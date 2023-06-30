f0d3b914842b61c39fbfb36b1d05825b07235378
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-30 03:00:16 -0300

    Actualizar LoadyUI.lua

--- Generaled/LoadyUI.lua
+++ Generaled/LoadyUI.lua
@@ -1,1 +1,12 @@
+local Library = loadstring(game:HttpGet("https://bitbucket.org/cat__/uis/raw/bb4228f2f970aad3c4a901161da5e678fb8e14ef/Module"))() 
+local Window = Library:Window("Loady", function(Script)
+print('Loaded', Script)
+end)
+Window:Game({
+    Status = true,
+    Script = "Hi",
+    Place = 3101667897
+})
+
+Window:Initiate()
 

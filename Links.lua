c0ce29a074096b84f17f0b18e1501980e96d1e10
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-29 21:08:02 -0300

    Actualizar Links.lua

--- Links.lua
+++ Links.lua
@@ -1,14 +1,21 @@
-ESP Players:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua
+ESP Players[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua
 Plataforma+10:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/%2B10DeAltura.lua
-Caminar sobre plataformas:https://github.com/AnonyProArg/ScriptsRobloz/blob/main/Generaled/CaminarPlataforma.lua
-Modo Espectador:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/ModoEspectador.lua
-Dex v3:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Dexv3.lua
-Atravesar Paredes:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Nocolision.lua
-Caer:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caer.lua
-Alejarse de jugadores:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejarse%2B.lua
-Laundry Simulator:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua
-Inmortalv1(God):https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20v4.txt
-Inmortalv2(God):https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20versi%C3%B3n%203.txt
-Inmortalv3(God):https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/InmortalV2.txt
-My Coffee Shop(Mí Cafetería):https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua
-Tycoon de leche(Mili):https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Tycoon%20Milk.lua
+Caminar sobre plataformas[Genelares]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/CaminarPlataforma.lua
+Modo Espectador[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/ModoEspectador.lua
+Dex v3[Tools]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Dexv3.lua
+Atravesar Paredes(noclip)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Nocolision.lua
+Caer[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caer.lua
+Alejarse de jugadores(Regresa)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejarse%2B.lua
+Laundry (Lavanderia)[Simulador]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua
+Inmortalv1(God)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20v4.txt
+Inmortalv2(God)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20versi%C3%B3n%203.txt
+Inmortalv3(God)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/InmortalV2.txt
+My Coffee Shop(Mí Cafetería)[Simulador]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua
+Tycoon de leche(Milk)[Tycoon]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Tycoon%20Milk.lua
+InfityYield (Comandos Hacks)[Tools]:https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source
+Spy Mobil[Tools]:https://raw.githubusercontent.com/HoodProject/Espa-oles/main/Turtle%20Spy.lua
+Reproductor Music ID[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Reproductor.lua
+Colocar Objetos en Casa[Meepcity]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/ColocarYenMeep.lua
+Menu para plantas [Meepcity]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/plantsMeepC.txt
+Donas(Donuts)[Tycoon]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/DonutsTycoon.lua
+Alejarse de jugadores(NoRegresa)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejar%2B10.lua

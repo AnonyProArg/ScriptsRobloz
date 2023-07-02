422b6a4399018c42990225d3cd65c7e67ae9f736
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-02 02:43:36 -0300

    Update Links.lua

--- Generaled/Links.lua
+++ Generaled/Links.lua
@@ -1,11 +1,10 @@
-
-  local Links = {
-        "true:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/ModoEspectador.lua",
-
-"true:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caminar%20en%20Plataforma.lua",
-
-"true:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua",
-  
-    }
-
-return Links
+{
+  "Generales/Tools":{
+    "Status":true,
+    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/OneCreatorX%203.lua"
+  },
+  "12398414727":{
+    "Status":true,
+    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua"
+  }
+}

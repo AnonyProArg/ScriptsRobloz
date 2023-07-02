59d3a9bd294540c686b071715a6738caf8b1412f
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-02 02:17:07 -0300

    Actualizar Math answer.lua

--- Math answer.lua
+++ Math answer.lua
@@ -1,1 +1,21 @@
+local Debounce = false
+
+game:GetService("ReplicatedStorage").Events.Round.RoundRE.OnClientEvent:Connect(function(Status, Time, Data)
+    if game:GetService("Players").LocalPlayer.InGame.Value then
+        if Status == "countdown" and not Debounce then
+            Debounce = true
+            Data.CurrentQuestion = Data.CurrentQuestion:gsub("= %?", "")
+
+            if Data.CurrentQuestion:match("÷") then
+                Data.CurrentQuestion = Data.CurrentQuestion:gsub("÷", "/")
+            elseif Data.CurrentQuestion:match("×") then
+                Data.CurrentQuestion = Data.CurrentQuestion:gsub("×", "*")
+            end
+
+            game:GetService("ReplicatedStorage").Functions.Answer.AnswerRF:InvokeServer("answer", loadstring("return "..Data.CurrentQuestion)())
+        elseif Status == "idle" then
+            Debounce = false
+        end
+    end
+end)
 

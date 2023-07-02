e0f89bc20d8e23fda18b657153b5074975b926f5
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-02 03:12:59 -0300

    Actualizar LoadyUI.lua

--- Generaled/LoadyUI.lua
+++ Generaled/LoadyUI.lua
@@ -5,35 +5,35 @@ end)
 {
   "Generales/Tools":{
     "Status":true,
-    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/OneCreatorX%203.lua"
+"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/OneCreatorX%203.lua"
   },
   "12398414727":{
     "Status":true,
-    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua"
+"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua"
   },
  "13276475159":{
     "Status":true,
-    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua"
+"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua"
   },
  "6305942109":{
-    "Status":true,
-
+    "Status":true"
 "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua"
   },
  "10108131074":{
     "Status":true,
-    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua"
+"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua"
   },
  "6915291292":{
     "Status":true,
-    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/ExePanaderia.lua"
+"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/ExePanaderia.lua"
   },
  "370731277":{
     "Status":true,
-    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Exes/MeepCity.lua"
+"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Exes/MeepCity.lua"
   },
  "12398414727":{
     "Status":true,
-    "Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua"
+"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua"
   }
+
 Window:Initiate()

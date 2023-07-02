0a3e6823740ff30637566fa4ba36e372d3d2a37b
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-02 04:32:53 -0300

    Actualizar plataforma1000x1000-300.lua

--- Generaled/plataforma1000x1000-300.lua
+++ Generaled/plataforma1000x1000-300.lua
@@ -1,10 +1,18 @@
-local Player = game.Players.LocalPlayer
-local Character = Player.Character or Player.CharacterAdded:Wait()
-local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
-
 local platformSize = Vector3.new(1000, 10, 1000) -- Tamaño de la plataforma
-local platformPosition = HumanoidRootPart.Position - Vector3.new(0, 40, 0) -- Posición de la plataforma
 
+-- Obtener el jugador local
+local player = game.Players.LocalPlayer
+
+-- Esperar a que el personaje del jugador esté disponible
+local character = player.Character or player.CharacterAdded:Wait()
+
+-- Esperar a que el punto de inicio del personaje esté disponible
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+
+-- Calcular la posición de la plataforma debajo del jugador
+local platformPosition = humanoidRootPart.Position - Vector3.new(0, 40, 0)
+
+-- Crear la plataforma
 local platform = Instance.new("Part")
 platform.Size = platformSize
 platform.Position = platformPosition
@@ -12,4 +20,3 @@ platform.BrickColor = BrickColor.new("Medium stone grey") -- Color de la platafo
 platform.Anchored = true
 platform.CanCollide = true
 platform.Parent = workspace
-

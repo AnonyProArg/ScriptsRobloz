8391d0d794ac220123535f7aa4ada451576a3a12
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-03 05:12:19 -0300

    Actualizar Links.lua

--- Links.lua
+++ Links.lua
@@ -1,32 +1,32 @@
-ESP Players::Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua
+ESP Players:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Esp.lua
 Plataforma+10:Utilidas:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/%2B10DeAltura.lua
-Caminar sobre plataformas::Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/CaminarPlataforma.lua
-Modo Espectador:::https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/ModoEspectador.lua
-Dex v3:::https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua
-Atravesar Paredes(noclip):::https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Nocolision.lua
-Caer[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caer.lua
-Alejarse de jugadores(Regresa)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejarse%2B.lua
-Laundry (Lavanderia)AutoGame[Simulador]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua
-Inmortalv1(God)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20v4.txt
-Inmortalv2(God)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20versi%C3%B3n%203.txt
-Inmortalv3(God)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/InmortalV2.txt
-My Coffee Shop(Mí Cafetería)[Simulador]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua
-Tycoon de leche(Milk)[Tycoon]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Tycoon%20Milk.lua
-InfityYield (Comandos Hacks)[Tools]:https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source
-Spy Mobil[Tools]:https://raw.githubusercontent.com/HoodProject/Espa-oles/main/Turtle%20Spy.lua
-Reproductor Music ID[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Reproductor.lua
-Colocar Objetos en Casa[Meepcity]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/ColocarYenMeep.lua
-Menu para plantas [Meepcity]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/plantsMeepC.txt
-Donas(Donuts)[Tycoon]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/DonutsTycoon.lua
-Alejarse de jugadores(NoRegresa)[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejar%2B10.lua
-Cortar Cesped(Mow The Lawn)[Simulador]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua
-Anti Afk[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/AntiAfk.lua
-Anti Censura 80%[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/NoCensure.lua
-Brillo/Luz de día/Fullbright[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Fullbright.lua
-Tpjugador cercano/Mod atack espalda [Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Tp%20jugador%20cercano.lua
-Tp Players[Generales]:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/TPplayers.lua
+Caminar sobre plataformas:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/CaminarPlataforma.lua
+Modo Espectador:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/ModoEspectador.lua
+Dex v3:Utilidad:Live:https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua
+Atravesar Paredes(noclip):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Nocolision.lua
+Caer:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caer.lua
+Alejarse de jugadores(Regresa):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejarse%2B.lua
+Laundry (Lavanderia)AutoGame:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua
+Inmortalv1(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20v4.txt
+Inmortalv2(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20versi%C3%B3n%203.txt
+Inmortalv3(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/InmortalV2.txt
+My Coffee Shop(Mí Cafetería)[Simulador]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua
+Tycoon de leche(Milk)[Tycoon]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Tycoon%20Milk.lua
+InfityYield (Comandos Hacks):Utilidad:Live:https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source
+Spy Mobil:Utilidad:Live:https://raw.githubusercontent.com/HoodProject/Espa-oles/main/Turtle%20Spy.lua
+Reproductor Music ID:Fun:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Reproductor.lua
+Colocar Objetos en Casa[Meepcity]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/ColocarYenMeep.lua
+Menu para plantas [Meepcity]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/plantsMeepC.txt
+Donas(Donuts)[Tycoon]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/DonutsTycoon.lua
+Alejarse de jugadores(NoRegresa):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejar%2B10.lua
+Cortar Cesped(Mow The Lawn)[Simulador]:Game:Process85%:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua
+Anti Afk:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/AntiAfk.lua
+Anti Censura 80% Chat:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/NoCensure.lua
+Brillo/Luz de día/Fullbright:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Fullbright.lua
+Tpjugador cercano/Mod atack espalda:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Tp%20jugador%20cercano.lua
+Tp Players:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/TPplayers.lua
 Waypoints TP-Position:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Waypoints.lua
-Generador Infin de plataformas:Fin:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/generador%20de%20plataformas.txt
+Generador Infin de plataformas:Fun:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/generador%20de%20plataformas.txt
 Generator plataforma InfV2:Fun:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Generate%20Plataformas%20InfV2.lua
 genera plataforma 1000x1000:Utilidad:Roto:https://github.com/AnonyProArg/ScriptsRobloz/blob/main/Generaled/plataforma1000x1000-300.lua
 Math answers o die:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua

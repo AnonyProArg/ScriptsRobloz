b7414d5e13042c1c1625422503c689b4f205ebb7
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-05 02:31:44 -0300

    Actualizar LinksLoadyUI.lua

--- Generaled/LinksLoadyUI.lua
+++ Generaled/LinksLoadyUI.lua
@@ -2,49 +2,49 @@
 "Updated":"04/07/2023",
   "Generales/Tools":{
      "Creator": "OneCreatorX",
-    "Status": true,
+    "Status": "Funcional y en Desarrollo",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/OneCreatorX%203.lua",
   "Updated": "04/07/2023"
   },
   "12398414727":{
-   "Creator": "OneCreatorX",
-    "Status": true,
+   "Creator": "Cat, OneCreatorX",
+    "Status": "Funcional",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua",
   "Updated": "04/07/2023"
   },
  "13276475159":{
     "Creator": "OneCreatorX",
-    "Status": true,
+    "Status": "Funcional y en Desarrollo",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua",
   "Updated": "04/07/2023"
   },
  "6305942109":{
     "Creator": "OneCreatorX",
-    "Status": true,
+    "Status": "Beta pero Funcional",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua",
   "Updated": "04/07/2023"
   },
  "10108131074":{
     "Creator": "OneCreatorX",
-    "Status": true,
+    "Status": "Funcional 1-8 mundos",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua",
   "Updated": "04/07/2023"
   },
  "6915291292":{
     "Creator": "OneCreatorX",
-    "Status": true,
+    "Status": "En Mantenimiento",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/ExePanaderia.lua",
   "Updated": "04/07/2023"
   },
  "370731277":{
     "Creator": "OneCreatorX",
-    "Status": true,
+    "Status": "Funcional",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Exes/MeepCity.lua",
   "Updated": "04/07/2023"
   },
  "12398414727":{
     "Creator": "OneCreatorX",
-    "Status": true,
+    "Status": "Funcional",
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua",
   "Updated": "04/07/2023"
   }

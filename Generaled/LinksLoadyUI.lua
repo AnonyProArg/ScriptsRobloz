9fc0ce5545c880da3e67a8fe086429fb77d1c870
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-05 01:38:31 -0300

    Actualizar LinksLoadyUI.lua

--- Generaled/LinksLoadyUI.lua
+++ Generaled/LinksLoadyUI.lua
@@ -4,48 +4,48 @@
      "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/OneCreatorX%203.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   },
   "12398414727":{
    "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   },
  "13276475159":{
     "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   },
  "6305942109":{
     "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   },
  "10108131074":{
     "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   },
  "6915291292":{
     "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/ExePanaderia.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   },
  "370731277":{
     "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Exes/MeepCity.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   },
  "12398414727":{
     "Creator": "OneCreatorX",
     "Status": true,
 "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua",
-  "Updated": "04/07/23"
+  "Updated": "04/07/2023"
   }
 }

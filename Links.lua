deebd8efd22e8aaa314b3b6e1759ca0431b6b3a4
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-07-05 01:59:17 -0300

    Update Links.lua

--- Links.lua
+++ Links.lua
@@ -7,8 +7,8 @@ Atravesar Paredes(noclip):Utilidad:Live:https://raw.githubusercontent.com/AnonyP
 Caer:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Caer.lua
 Alejarse de jugadores(Regresa):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejarse%2B.lua
 Laundry (Lavanderia)AutoGame:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua
-Inmortalv1(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20v4.txt
-Inmortalv2(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20versi%C3%B3n%203.txt
+Inmortalv1(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20v4.lua
+Inmortalv2(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/inmortal%20versi%C3%B3n%203.lua
 Inmortalv3(God):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/InmortalV2.txt
 My Coffee Shop(Mí Cafetería)[Simulador]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua
 Tycoon de leche(Milk)[Tycoon]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Tycoon%20Milk.lua
@@ -16,7 +16,7 @@ InfityYield (Comandos Hacks):Utilidad:Live:https://raw.githubusercontent.com/Edg
 Spy Mobil:Utilidad:Live:https://raw.githubusercontent.com/HoodProject/Espa-oles/main/Turtle%20Spy.lua
 Reproductor Music ID:Fun:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Reproductor.lua
 Colocar Objetos en Casa[Meepcity]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/ColocarYenMeep.lua
-Menu para plantas [Meepcity]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/plantsMeepC.txt
+Menu para plantas [Meepcity]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/plantsMeepC.lua
 Donas(Donuts)[Tycoon]:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/DonutsTycoon.lua
 Alejarse de jugadores(NoRegresa):Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Alejar%2B10.lua
 Cortar Cesped(Mow The Lawn)[Simulador]:Game:Process85%:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua
@@ -26,7 +26,7 @@ Brillo/Luz de día/Fullbright:Utilidad:Live:https://raw.githubusercontent.com/An
 Tpjugador cercano/Mod atack espalda:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Tp%20jugador%20cercano.lua
 Tp Players:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/TPplayers.lua
 Waypoints TP-Position:Utilidad:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Waypoints.lua
-Generador Infin de plataformas:Fun:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/generador%20de%20plataformas.txt
+Generador Infin de plataformas:Fun:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/generador%20de%20plataformas.lua
 Generator plataforma InfV2:Fun:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/Generate%20Plataformas%20InfV2.lua
 Evitar Caída Digite - :Utilidad:Beta:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Generaled/plataforma1000x1000-300.lua
 Math answers o die:Game:Live:https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua
